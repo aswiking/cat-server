6757b0b8c9b571fb221f88dbca964ab06d821a26
Sofia Wiking <aswiking@gmail.com>
2020-03-23 15:03:39 +0000

    add validation schema

---
+++ src/routers/cat-schema.js
@@ -0,0 +1,11 @@
+import Joi from '@hapi/joi';
+
+const catSchema = Joi.object({
+    id: Joi.string().min(1),
+    name: Joi.string().min(1).required(),
+    size: Joi.string().required(),
+    mood: Joi.string().required(),
+    imageLocation: 
+
+
+})
